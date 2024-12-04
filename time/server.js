3e89052738d3475571430f23c304373fd901ca8a
keaideweiertuode <59241730+keaideweiertuode@users.noreply.github.com>
2024-12-04 20:15:47 +0800

    Add files via upload

---
+++ time/server.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        const filePath = path.join(__dirname, 'index.html');
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Server Error');
+            } else {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content);
+            }
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const PORT = 3001;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
